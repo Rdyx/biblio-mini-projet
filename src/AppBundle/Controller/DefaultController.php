d0d0ce12e2f35c61babafd31fd200b04f3d4a5e0
Rdyx <tiranico.pro@gmail.com>
2017-11-28 11:28:36 +0100

    start

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request = null)
+    {
+        return new Response('<a href="http://127.0.0.1:8000/app_dev.php/login">login</a>');
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+}
